833c2ac151254223069259f64afad8204f46a413
Boka <bokanet@gmail.com>
2016-01-09 17:43:26 +0200

    v1

---
+++ src/TestJava/Arra.java
@@ -0,0 +1,16 @@
+package TestJava;
+
+/**
+ * Created by Boka on 2016.01.09..
+ */
+public class Arra {
+
+    public static void main(String[] args){
+
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+        double[] myDoubleArray = new double[10];
+        System.out.println(myIntArray[5]);
+
+    }
+}
