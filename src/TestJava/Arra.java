f0a062942a50d70045ac6a849dc85752bd48bff8
Boka <bokanet@gmail.com>
2016-01-09 18:32:28 +0200

    v1.1

--- src/TestJava/Arra.java
+++ src/TestJava/Arra.java
@@ -1,16 +1,37 @@
 package TestJava;
 
+import java.util.Scanner;
+
 /**
  * Created by Boka on 2016.01.09..
  */
 public class Arra {
 
-    public static void main(String[] args){
+    private static Scanner scanner = new Scanner(System.in);
 
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;
-        double[] myDoubleArray = new double[10];
-        System.out.println(myIntArray[5]);
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for(int i=0;i<myIntegers.length;i++){
+            System.out.println("Element " + i +", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
 
+    }
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0;i<values.length;i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for(int i=0;i<array.length;i++){
+            sum += array[i];
+        }
+        return (double) sum / (double)array.length;
     }
 }
