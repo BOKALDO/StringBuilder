1aa246541c0955bca4f46bfeb6c7ffe0147a8485
Boka <bokanet@gmail.com>
2016-01-09 13:17:02 +0200

    v1.1

--- src/TestJava/Polimorphism.java
+++ src/TestJava/Polimorphism.java
@@ -16,6 +16,55 @@ class Movie{
     }
 }
 
+class Jaws extends Movie{
+    public  Jaws(){
+        super("Jaws");
+    }
+    public String plot(){
+        return "Visi ir Dead";
+    }
+}
+
+class AlienDay extends Movie{
+    public AlienDay(){
+        super("Alien Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens take you over";
+    }
+}
+
+class MazeRunner extends Movie{
+    public MazeRunner(){
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids escape wilderness."
+    }
+}
+
+class StarWars extends Movie{
+    public StarWars(){
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Joda with tricks";
+    }
+}
+
+class Forgettable extends Movie{
+    public Forgettable() {
+        super("Forgettable");
+    }
+    //tukshums
+}
+
 public class Polimorphism {
     public static void main(String[] args){
 
