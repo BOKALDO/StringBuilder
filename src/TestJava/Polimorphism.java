c13128fe44854305ce6aa1a569ed95ac262b0dec
Boka <bokanet@gmail.com>
2016-01-09 12:55:08 +0200

    v1

---
+++ src/TestJava/Polimorphism.java
@@ -0,0 +1,23 @@
+package TestJava;
+
+/**
+ * Created by Boka on 2016.01.09..
+ */
+
+class Movie{
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot(){
+        return "No plot";
+    }
+}
+
+public class Polimorphism {
+    public static void main(String[] args){
+
+    }
+}
